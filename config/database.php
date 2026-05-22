512d0dc08f26a9168a2e9856161b72100ed3f10f
Raul Escalante <raulescalantem14@gmail.com>
2026-05-22 10:17:44 -0500

    Test Railway

--- config/database.php
+++ config/database.php
@@ -1,26 +1,42 @@
 <?php
 
 class Database {
-    private $host = "localhost";
-    private $db_name = "club_amper";
-    private $username = "root";
-    private $password = "";
+
+    private $host = $_ENV['MYSQLHOST'];
+
+    private $db_name = $_ENV['MYSQLDATABASE'];
+
+    private $username = $_ENV['MYSQLUSER'];
+
+    private $password = $_ENV['MYSQLPASSWORD'];
+
+    private $port = $_ENV['MYSQLPORT'];
+
     public $conn;
 
     public function getConnection() {
+
         $this->conn = null;
 
         try {
+
             $this->conn = new PDO(
-                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                "mysql:host=" . $this->host .
+                ";port=" . $this->port .
+                ";dbname=" . $this->db_name,
                 $this->username,
                 $this->password
             );
 
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn->setAttribute(
+                PDO::ATTR_ERRMODE,
+                PDO::ERRMODE_EXCEPTION
+            );
 
         } catch (PDOException $exception) {
-            echo "Error de conexión: " . $exception->getMessage();
+
+            echo "Error de conexión: " .
+                 $exception->getMessage();
         }
 
         return $this->conn;
