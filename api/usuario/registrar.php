1bb044249082c403df7160e1e6d3af20726cf037
Raul Escalante <raulescalantem14@gmail.com>
2026-05-26 11:12:21 -0500

    Fix production image paths

--- api/usuario/registrar.php
+++ api/usuario/registrar.php
@@ -1,15 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Access-Control-Allow-Methods: POST, OPTIONS");
-header("Content-Type: application/json");
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit;
-}
-
-require_once __DIR__ . "/../../config/database.php";
+require_once "../../config/bootstrap.php";
 require_once __DIR__ . "/../../controllers/UsuarioController.php";
 
 $db = new Database();
